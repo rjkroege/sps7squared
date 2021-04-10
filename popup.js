c41f224b1095b37e2e5d64af0c078cf1613e23d2
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2021-04-09 20:48:34 -0700

    Format commenting according to Google JavaScript guidelines

--- popup.js
+++ popup.js
@@ -6,6 +6,7 @@ window.onload = function () {
     var text = "";
 
     /* Creates a form that allows for the user to input text to later be used in a note. */
+
     plusNote.onclick = function writeNote() {
         noteForm.classList.toggle("show");
         noteForm.reset();
@@ -13,40 +14,52 @@ window.onload = function () {
         if (!plusNote.classList.contains("bi-x-circle")) {
             plusNote.classList.add("bi-x-circle");
             plusNote.classList.remove("bi-plus-circle");
-        }
-        else {
+        } else {
             plusNote.classList.remove("bi-x-circle");
             plusNote.classList.add("bi-plus-circle");
         }
     };
 
-    /* Creates an instance of a note using the text submission from noteForm. Prevents the deafult action of form submission in order to grab contents of the textarea. */
+    /* Creates an instance of a note using the text submission from noteForm. Prevents the 
+     * default action of form submission in order to grab contents of the textarea. */
+
     submitNote.onclick = function addNote(ev) {
-        ev.preventDefault(); //stop the form submitting
+        
+        //stop the form submitting
+
+        ev.preventDefault(); 
 
         let note = {
-            text: document.getElementById("text").value
-        }
+            text: document.getElementById("text").value,
+        };
         text = note.text;
 
         noteForm.reset();
 
         noteForm.classList.remove("show");
 
-        var newNote = document.createElement('div');
+        var newNote = document.createElement("div");
         newNote.setAttribute("class", "note");
 
-        /* Adds in the inner html that is basically the note. CSS takes these elements into account. Text variable holds the text from the above function that creates an instance of a note */
-        newNote.innerHTML = "<div class='text'>" + text + "</div><div class='edits-container'><i id='edit' class='bi bi-pencil'></i><i id='delete' class='bi bi-trash'></i></div>";
+        //Adds in the inner html that is basically the note. CSS takes these elements into 
+        //account. Text variable holds the text from the above function that creates an
+        //instance of a note
 
-        /* appends child (newNote) to the top */
+        newNote.innerHTML =
+            "<div class='text'>" +
+            text +
+            "</div><div class='edits-container'><i id='edit' class='bi bi-pencil'></i><i id='delete' class='bi bi-trash'></i></div>";
+
+        //Appends child (newNote) to the top
+
         var container = document.getElementsByClassName("notes-container");
         container[0].prepend(newNote);
 
-        /* Takes care of consistency in the plus vs x button for creating/cancelling a note */
+        //Takes care of consistency in the plus vs x button for creating/cancelling a note
+
         if (plusNote.classList.contains("bi-x-circle")) {
             plusNote.classList.remove("bi-x-circle");
             plusNote.classList.add("bi-plus-circle");
         }
-    }
+    };
 };
