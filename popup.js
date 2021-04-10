10de7fa1285a2ffb25928f373aff37814e16c773
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2021-04-09 22:26:26 -0700

    Add global class to hold global state. All global variables held in a constructor

--- popup.js
+++ popup.js
@@ -1,65 +1,73 @@
 window.onload = function () {
-    /* GLOBAL VARIABLES: form, add, submit, and text grabbed from submission of the form. */
-    var noteForm = document.getElementById("noteForm");
-    var plusNote = document.getElementById("add");
-    var submitNote = document.getElementById("submit");
-    var text = "";
+    
+    /* GLOBAL VARIABLES: form, add, submit, and text grabbed from submission of the form.
+     * Held in a global class for accessibility and holding global state */
+
+    class Global {
+        constructor() {
+            this.noteForm = document.getElementById("noteForm");
+            this.plusNote = document.getElementById("add");
+            this.submitNote = document.getElementById("submit");
+            this.text = "";
+        }
+    }
+
+    var global = new Global();
 
     /* Creates a form that allows for the user to input text to later be used in a note. */
 
-    plusNote.onclick = function writeNote() {
-        noteForm.classList.toggle("show");
-        noteForm.reset();
+    global.plusNote.onclick = function writeNote() {
+        global.noteForm.classList.toggle("show");
+        global.noteForm.reset();
 
-        if (!plusNote.classList.contains("bi-x-circle")) {
-            plusNote.classList.add("bi-x-circle");
-            plusNote.classList.remove("bi-plus-circle");
+        if (!global.plusNote.classList.contains("bi-x-circle")) {
+            global.plusNote.classList.add("bi-x-circle");
+            global.plusNote.classList.remove("bi-plus-circle");
         } else {
-            plusNote.classList.remove("bi-x-circle");
-            plusNote.classList.add("bi-plus-circle");
+            global.plusNote.classList.remove("bi-x-circle");
+            global.plusNote.classList.add("bi-plus-circle");
         }
     };
 
     /* Creates an instance of a note using the text submission from noteForm. Prevents the 
      * default action of form submission in order to grab contents of the textarea. */
 
-    submitNote.onclick = function addNote(ev) {
-        
-        //stop the form submitting
+    global.submitNote.onclick = function addNote(ev) {
 
-        ev.preventDefault(); 
+        //Stop the form submitting.
+
+        ev.preventDefault();
 
         let note = {
             text: document.getElementById("text").value,
         };
-        text = note.text;
-
-        noteForm.reset();
-
-        noteForm.classList.remove("show");
+        global.text = note.text;
+        
+        global.noteForm.reset();
+        global.noteForm.classList.remove("show");
 
         var newNote = document.createElement("div");
         newNote.setAttribute("class", "note");
 
         //Adds in the inner html that is basically the note. CSS takes these elements into 
         //account. Text variable holds the text from the above function that creates an
-        //instance of a note
+        //instance of a note.
 
         newNote.innerHTML =
             "<div class='text'>" +
-            text +
+            global.text +
             "</div><div class='edits-container'><i id='edit' class='bi bi-pencil'></i><i id='delete' class='bi bi-trash'></i></div>";
 
-        //Appends child (newNote) to the top
+        //Appends child (newNote) to the top.
 
         var container = document.getElementsByClassName("notes-container");
         container[0].prepend(newNote);
 
-        //Takes care of consistency in the plus vs x button for creating/cancelling a note
+        //Takes care of consistency in the plus vs x button for creating/cancelling a note.
 
-        if (plusNote.classList.contains("bi-x-circle")) {
-            plusNote.classList.remove("bi-x-circle");
-            plusNote.classList.add("bi-plus-circle");
+        if (global.plusNote.classList.contains("bi-x-circle")) {
+            global.plusNote.classList.remove("bi-x-circle");
+            global.plusNote.classList.add("bi-plus-circle");
         }
     };
 };
