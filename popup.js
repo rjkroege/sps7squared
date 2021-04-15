dcda400ed250ed80921183d904287067ebf0f7df
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-04-14 22:39:28 -0700

    Fix path

--- popup.js
+++ popup.js
@@ -67,7 +67,7 @@ window.onload = function () {
                 // Sign-out successful.
                 chrome.runtime.sendMessage({ message: "sign_out" }, function (responese) {
                     if (responese.message === "success") {
-                      window.location.replace("./login.html");
+                      window.location.replace("login.html");
                     }
                   });
                 //alert("Sign-out successful");
