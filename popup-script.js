ddf8096485c1bb5d14b18d3912a2f7c84ab2acf1
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-04-14 21:56:49 -0700

    Add log out logic to button and get rid of extra file

--- popup-script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector("#sign_out").addEventListener("click", () => {
-  chrome.runtime.sendMessage({ message: "sign_out" }, function (responese) {
-    if (responese.message === "success") {
-      window.location.replace("./login.html");
-    }
-  });
-});
--- popup.js
+++ popup.js
@@ -65,7 +65,12 @@ window.onload = function () {
             .signOut()
             .then(() => {
                 // Sign-out successful.
-                alert("Sign-out successful");
+                chrome.runtime.sendMessage({ message: "sign_out" }, function (responese) {
+                    if (responese.message === "success") {
+                      window.location.replace("./login.html");
+                    }
+                  });
+                //alert("Sign-out successful");
             })
             .catch((error) => {
                 // An error happened.
