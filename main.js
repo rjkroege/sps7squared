509c63e48bdff4c9324ba9899cf49f773ee5ac2d
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2021-04-15 13:51:00 -0700

    Refactor firebase config

--- main.js
+++ main.js
@@ -58,3 +58,15 @@ function logout() {
       console.log(error);
     });
 }
+
+function makeFirebaseConfig() {
+  return {
+    apiKey: config().apiKey,
+    authDomain: config().authDomain,
+    projectId: config().projectId,
+    storageBucket: config().storageBucket,
+    messagingSenderId: config().messagingSenderId,
+    appId: config().appId,
+    measurementId: config().measurementId,
+  };
+}
